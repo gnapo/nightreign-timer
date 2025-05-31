4f2d84805fae0a88c5ef237abad5b8567ecc5dd9
Fabian <fabian.stroh@gmail.com>
2025-06-01 01:55:47 +0200

    make screen stay on

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import  Timer  from './components/Timer.vue'
+import {useWakeLock} from "@vueuse/core";
+import {reactive} from "vue"
+const wakelock = reactive(useWakeLock())
+wakelock.request('screen')
+
 </script>
 
 <template>
